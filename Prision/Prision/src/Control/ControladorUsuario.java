2139f8b0ccb2589a41518b7359493e59e1211783
GIAMPAOLO DELGADO <57118959+DEATHSHOOTERgd@users.noreply.github.com>
2021-07-04 21:36:53 -0500

    Add files via upload

---
+++ Prision/Prision/src/Control/ControladorUsuario.java
@@ -0,0 +1,16 @@
+
+package Control;
+
+
+public class ControladorUsuario {
+    public Object buscarUsuario(String nombre)
+    {
+        return null;
+    }
+    public String validarCredencial(String nombreUsuario, char[] password)
+    {
+    return null;
+    }
+    
+    
+}
